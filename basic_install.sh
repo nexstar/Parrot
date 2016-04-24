a0a2ca7e830c540e48ab173aa9ae0a25bed7b742
nexstar <nexstar1436@gmail.com>
2016-04-23 22:49:38 -0700

    Bash

---
+++ basic_install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+sudo apt-get update
+echo "**********install tools**********"
+sudo apt-get install open-server vim git wget automake build-essential  autoconf libtool yasm nasm phablet-tools repo -y
+echo "**********install lib************"
+sudo apt-get install libncurses5-dev libavcodec-dev libavformat-dev libswscale-dev libavutil-dev -y
+sudo apt-get install libavahi-core-dev libavahi-client-dev libjson0-dev libjson0 mplayer -y
+echo "******build fly and sync*********"
+mkdir ~/fly5/bebop -p && cd ~/fly5/bebop
+repo init -u https://github.com/Parrot-Developers/arsdk_manifests.git
+repo sync
+
+export LD_LIBRARY_PATH=~/fly5/bebop/out/Unix-base/staging/usr/lib:$LD_LIBRARY_PATH
+		      
+echo $LD_LIBRARY_PATH
+
+cd ~/fly5/bebop
+./build.sh -p arsdk-native -t build-sdk -j
+
