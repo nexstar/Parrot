4fddcbdf0ec014b855b57c18a0226bee0744832a
nexstar <nexstar1436@gmail.com>
2016-05-04 19:51:34 +0800

    upAndroid

--- Android/Install_Studio.sh
+++ Android/Install_Studio.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java7-installer oracle-java7-set-default
+sudo apt-get install oracle-java7-installer oracle-java7-set-default -y
 sudo apt-add-repository -y ppa:maarten-fonville/android-studio  
 sudo apt-get update
 sudo apt-get install android-studio -y
