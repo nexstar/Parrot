3c17aa1e0064182be76872848de000d558429edd
derrick <drose11244@gmail.com>
2016-05-24 15:03:55 +0800

    add commm for samplecv

--- Unix/Samples/SampleCV/faceDectecor.py
+++ Unix/Samples/SampleCV/faceDectecor.py
@@ -4,25 +4,27 @@
 from SimpleCV import *
 from time import sleep
 
-#def
+
+#camera image size
 myCamera = Camera(prop_set={'width':320, 'height': 240})
+#windows size
 myDisplay = Display(resolution=(320, 240))
 
-#work
+#
 while not myDisplay.isDone():
-   frame = myCamera.getImage()
-   faces = frame.findHaarFeatures('face.xml') #find xml
+   frame = myCamera.getImage()#get camera image
+   faces = frame.findHaarFeatures('face.xml') #find xml get face
    if faces:
      for face in faces:
-        print "Face at: " + str(face.coordinates())
-        facelayer = DrawingLayer((frame.width,frame.height))
+        print "Face at: " + str(face.coordinates())#face center posite
+        facelayer = DrawingLayer((frame.width,frame.height))#new draw in windows
         w=face.width()
         h=face.height()
         print "x:"+str(w)+" y:"+str(h)
         facebox_dim = (w,h)
-        facebox = facelayer.centeredRectangle(face.coordinates(),facebox_dim)
-        frame.addDrawingLayer(facelayer)
-        frame.applyLayers()
+        facebox = facelayer.centeredRectangle(face.coordinates(),facebox_dim)#draw square in windows
+        frame.addDrawingLayer(facelayer)#add draw input 
+        frame.applyLayers()#output in windows 
    else:
         print "No faces detected."
    frame.save(myDisplay)
